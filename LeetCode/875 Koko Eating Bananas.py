7a072e0812b7c18c87c34e30441a2b96fbed7e7f
Robyn Lam <robynlamgy@gmail.com>
2022-07-12 23:17:31 -0700

    875

--- LeetCode/875 Koko Eating Bananas.py
+++ LeetCode/875 Koko Eating Bananas.py
@@ -1,11 +1,3 @@
-# Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. The guards have gone and will come back in h hours.
-
-# Koko can decide her bananas-per-hour eating speed of k. Each hour, she chooses some pile of bananas and eats k bananas from that pile. If the pile has less than k bananas, she eats all of them instead and will not eat any more bananas during this hour.
-
-# Koko likes to eat slowly but still wants to finish eating all the bananas before the guards return.
-
-# Return the minimum integer k such that she can eat all the bananas within h hours.
-
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         # min bananas/hour to finish all bananas within h hours
@@ -25,7 +17,8 @@ class Solution:
         def isValid(speed):
             count = 0
             for pile in piles:
-                count += math.ceil(pile / speed)
+                count += ((pile-1)//speed)+1 # <== faster than math.ceil
+                # count += math.ceil(pile / speed)
                 if count > h:
                     return False
             return True
@@ -38,3 +31,20 @@ class Solution:
                 l = m + 1
 
         return l
+# class Solution:
+#     def minEatingSpeed(self, piles: List[int], H: int) -> int:
+#         l, r = 1, max(piles)
+#         k = 0
+
+#         while l <= r:
+#             m = (l + r) // 2
+
+#             totalTime = 0
+#             for p in piles:
+#                 totalTime += ((p-1)//m) + 1
+#             if totalTime <= H:
+#                 k = m
+#                 r = m - 1
+#             else:
+#                 l = m + 1
+#         return k
