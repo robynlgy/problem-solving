cbf5661e823c9f1dc766a6661ea8b9a47219ee04
Robyn Lam <robynlamgy@gmail.com>
2022-07-26 14:59:32 -0700

    200

--- LeetCode/200 Number of Islands.py
+++ LeetCode/200 Number of Islands.py
@@ -19,32 +19,55 @@
 #   ["0","0","0","1","1"]
 # ]
 # Output: 3
-
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        ROWS , COLS = len(grid), len(grid[0])
+        seen = set()
         count = 0
 
-        def dfs(r,c):
-            # print("beg",r,c)
-            # note: instead of mutating grid, could implement a seen set passing in coords
-            grid[r][c] = '#'
-            directions = [[-1,0],[1,0],[0,-1],[0,1]]
-            for dr, dc in directions:
-                _r = r + dr
-                _c = c + dc
-                # print("next",_r,_c)
-                if (_r in range(len(grid)) and
-                    _c in range(len(grid[0])) and
-                    grid[_r][_c] == '1'):
-                    dfs(_r,_c)
+        def dfs(i,j):
+            if (i < 0 or i>= ROWS or
+                j < 0 or j>= COLS or
+                (i,j) in seen or
+                grid[i][j] == '0'):
+                return False
 
-        for r in range(len(grid)):
-            for c in range(len(grid[0])):
-                if grid[r][c] == '1':
-                    dfs(r,c)
+            seen.add((i,j))
+            dfs(i-1,j)
+            dfs(i+1,j)
+            dfs(i,j-1)
+            dfs(i,j+1)
+            return True
+
+        for i in range(ROWS):
+            for j in range(COLS):
+                if dfs(i,j):
                     count += 1
-        # print(grid)
+
         return count
+
+#         count = 0
+
+#         def dfs(r,c):
+#             # print("beg",r,c)
+#             # note: instead of mutating grid, could implement a seen set passing in coords
+#             grid[r][c] = '#'
+#             directions = [[-1,0],[1,0],[0,-1],[0,1]]
+#             for dr, dc in directions:
+#                 _r = r + dr
+#                 _c = c + dc
+#                 # print("next",_r,_c)
+#                 if (_r in range(len(grid)) and
+#                     _c in range(len(grid[0])) and
+#                     grid[_r][_c] == '1'):
+#                     dfs(_r,_c)
+
+#         for r in range(len(grid)):
+#             for c in range(len(grid[0])):
+#                 if grid[r][c] == '1':
+#                     dfs(r,c)
+#                     count += 1
+#         return count
 
 
 
